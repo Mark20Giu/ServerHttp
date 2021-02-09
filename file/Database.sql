80defcea007f04416020c673a6d93808a4782360
Informatica <giuliani.marco@gmail.com>
2021-02-09 17:56:17 +0100

    Aggiunto file configurazione database

---
+++ file/Database.sql
@@ -0,0 +1,17 @@
+drop database if exists tpsit;
+use tpsit;
+create table if not exists Alunni(
+	id int not null,
+	nome varchar(40) not null,
+    cognome varchar(40) not null,
+    primary key (idstudente)
+);
+insert into Alunni (id, nome, cognome)
+values (1,"Aurora","Trentini"),
+(2,"Ladislao","Ricci"),
+(3,"Lucio","Capon"),
+(4,"Selene","Costa"),
+(5,"Bonifacio","Marino"),
+(6,"Pietro","Colombo"),
+(7,"Tranquillo","Udinesi"),
+(8,"Greta","Buccho");
